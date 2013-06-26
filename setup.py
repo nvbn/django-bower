c34a7383ded43181c96390d63e711eefe5232a87
nvbn <nvbn.rm@gmail.com>
2013-06-26 04:51:16 +0400

    Reformat setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,32 @@
 from setuptools import setup, find_packages
-import sys, os
 
 version = '3'
 
-setup(name='django-bower',
-      version=version,
-      description="Integrate django with bower",
-      long_description="""\
-      Easy way to use bower with your django project
+setup(
+    name='django-bower',
+    version=version,
+    description="Integrate django with bower",
+    long_description="""\
+    Easy way to use bower with your django project
 """,
-      classifiers=[
-          'Framework :: Django',
-          'Programming Language :: JavaScript',
-      ],
-      keywords='',
-      author='Vladimir Iakovlev',
-      author_email='nvbn.rm@gmail.com',
-      url='https://github.com/nvbn/django-bower',
-      license='BSD',
-      packages=find_packages(exclude=['ez_setup', 'example', 'tests']),
-      include_package_data=True,
-      zip_safe=True,
-      install_requires=[
-          'django',
-          'mock',
-          'six',
-      ],
-      entry_points="""
+    classifiers=[
+        'Framework :: Django',
+        'Programming Language :: JavaScript',
+    ],
+    keywords='',
+    author='Vladimir Iakovlev',
+    author_email='nvbn.rm@gmail.com',
+    url='https://github.com/nvbn/django-bower',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup', 'example', 'tests']),
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'django',
+        'mock',
+        'six',
+    ],
+    entry_points="""
       # -*- Entry points: -*-
       """,
-      )
+)
