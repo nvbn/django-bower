44dbe4948d6f0b959f41cf0f2f3b9949eede14ec
nvbn <nvbn.rm@gmail.com>
2013-09-09 07:37:56 +0400

    Fix pep8

--- example/example/urls.py
+++ example/example/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^$', TemplateView.as_view(template_name='index.html')),
 )
--- runtests.py
+++ runtests.py
@@ -3,7 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'djangobower.test_settings')
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", 'djangobower.test_settings',
+    )
 
     from django.core.management import execute_from_command_line
 
