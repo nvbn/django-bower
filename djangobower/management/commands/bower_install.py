345a1693e9559f19ec7e17877193ea8ad68ba362
Michał Pasternak <michal.dtz@gmail.com>
2014-07-11 16:00:38 +0200

    Force conflict resolution
    In my case, I always get that annoying message about incompatible jquery
    versions, so this should help. There are other cool bower options I
    presume, but this is everything I need for now.

--- djangobower/management/commands/bower_install.py
+++ djangobower/management/commands/bower_install.py
@@ -1,9 +1,21 @@
+from optparse import make_option
 from ..base import BaseBowerCommand
 
 
 class Command(BaseBowerCommand):
     help = 'Install bower apps'
 
+    option_list = BaseBowerCommand.option_list + (
+        make_option('-F',
+                    action='store_true',
+                    dest='force',
+                    default=False,
+                    help='Force installation of latest package version on conflict'),
+    )
+
     def handle(self, *args, **options):
         super(Command, self).handle(*args, **options)
+
+        if options.get('force') == True:
+            args = args + ("-F", )
         self._install(args)
