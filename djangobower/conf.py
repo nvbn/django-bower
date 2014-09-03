f7c2296517772d63e935a4c4774be4f51116ae38
Capi Etheriel <barraponto@gmail.com>
2014-09-03 13:57:46 -0300

    Fix bower_components path to parent path.

--- djangobower/conf.py
+++ djangobower/conf.py
@@ -2,7 +2,7 @@ import os, sys
 
 from django.conf import settings
 
-COMPONENTS_ROOT = getattr(settings, 'BOWER_COMPONENTS_ROOT', os.path.join(settings.BASE_DIR, 'bower_components'))
+COMPONENTS_ROOT = getattr(settings, 'BOWER_COMPONENTS_ROOT', settings.BASE_DIR)
 
 BOWER_PATH_DEFAULT = 'bower'
 if sys.platform == 'win32':
