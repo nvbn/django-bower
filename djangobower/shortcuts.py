66235da5cbdc5d1635f538b410b94c37654f3037
Michał Pasternak <michal.dtz@gmail.com>
2014-10-22 13:29:29 +0200

    More win32 support!

--- djangobower/shortcuts.py
+++ djangobower/shortcuts.py
@@ -1,8 +1,11 @@
-import os
+import os, sys
 
 
 def is_executable(path):
     """Check file is executable"""
+    if sys.platform == 'win32':
+        executable = os.access(path, os.X_OK) or path.lower().endswith(".cmd")
+        return os.path.isfile(path) and executable
     return os.path.isfile(path) and os.access(path, os.X_OK)
 
 
