bb57b3ade383cf90890d3ee86deb6c3cf1015108
Jacob Rief <jacob.rief@gmail.com>
2015-01-21 08:56:02 +0100

    Fixes issue #28
    make some additional PEP8 cleanup
    do not export local variables from the module

--- djangobower/conf.py
+++ djangobower/conf.py
@@ -1,11 +1,15 @@
-import os, sys
-
+# -*- coding: utf-8 -*-
+import os
+import sys
 from django.conf import settings
 
-COMPONENTS_ROOT = getattr(settings, 'BOWER_COMPONENTS_ROOT', settings.BASE_DIR)
+__all__ = ['COMPONENTS_ROOT', 'BOWER_PATH']
 
-BOWER_PATH_DEFAULT = 'bower'
+COMPONENTS_ROOT = getattr(settings, 'BOWER_COMPONENTS_ROOT', os.path.abspath(os.path.dirname(__name__)))
+
 if sys.platform == 'win32':
-	BOWER_PATH_DEFAULT = os.path.join(os.getenv("APPDATA"), 'npm', 'bower.cmd')
+    default_bower_path = os.path.join(os.getenv("APPDATA"), 'npm', 'bower.cmd')
+else:
+    default_bower_path = 'bower'
 
-BOWER_PATH = getattr(settings, 'BOWER_PATH', BOWER_PATH_DEFAULT)
+BOWER_PATH = getattr(settings, 'BOWER_PATH', default_bower_path)
